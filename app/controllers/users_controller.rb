c5ea215b340783f2495ddb072eef57758158d741
YC Teoh <ycteoh@YCs-MBA.local>
2023-03-14 17:03:32 +0800

    HTML Form concluded

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,33 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
+    # ------ HTML Form (5) ------ #
+    # ------ Commented out because of HTML Form (10) ------ #
+    # @user = User.new(
+    #   username: params[:username],
+    #   email: params[:email],
+    #   password: params[:password]
+    # )
 
+    # ------ HTML Form (12) ------ #
+    @user = User.new(user_params)
+
+    # HTML Form (5)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  # ------ HTML Form (11) ------ #
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+  private :user_params
 
 end
